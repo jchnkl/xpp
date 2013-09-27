c2044dbf08ac6a1f7f0394bc4c53a24de13ecbff
Jochen Keil <jochen.keil@gmail.com>
2013-09-27 22:52:43 +0200

    Templated wrapper for xcb request/reply
    First access to reply also fetches it.
    Initial commit.

---
+++ request.hpp
@@ -0,0 +1,44 @@
+#ifndef X_REQUEST_HPP
+#define X_REQUEST_HPP
+
+#include <memory>
+#include <xcb/xcb.h>
+
+namespace x {
+
+template<typename COOKIE,
+         typename REPLY,
+         REPLY * (*REPLY_FUN)(xcb_connection_t *, COOKIE, xcb_generic_error_t **)>
+class request {
+  public:
+    template<typename ... COOKIE_ARGS>
+    request(xcb_connection_t * const c,
+            COOKIE (*cookie_fun)(xcb_connection_t *, COOKIE_ARGS ...),
+            COOKIE_ARGS ... cookie_args)
+      : m_c(c)
+    {
+      m_cookie = cookie_fun(c, cookie_args ...);
+    }
+
+    const REPLY & operator*(void)
+    {
+      if (! m_reply) {
+        m_reply = std::shared_ptr<REPLY>(REPLY_FUN(m_c, m_cookie, NULL));
+      }
+      return *m_reply.get();
+    }
+
+    const REPLY * const operator->(void)
+    {
+      return &*(*this);
+    }
+
+  private:
+    xcb_connection_t * m_c;
+    COOKIE m_cookie;
+    std::shared_ptr<REPLY> m_reply;
+};
+
+}; // namespace x
+
+#endif // X_REQUEST_HPP
