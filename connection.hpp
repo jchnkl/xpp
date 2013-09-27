6f5f971c4fe3b6c8a503fb9d49de6e0e13e64818
Jochen Keil <jochen.keil@gmail.com>
2013-09-27 23:27:59 +0200

    Add default root window & default screen to connection

--- connection.hpp
+++ connection.hpp
@@ -10,10 +10,12 @@ class connection {
     connection(const std::string & displayname)
     {
       m_c = xcb_connect(displayname.c_str(), &m_default_screen_number);
+      m_default_screen_of_display = screen_of_display(m_default_screen_number);
     }
 
     ~connection(void)
     {
+      delete m_default_screen_of_display;
       xcb_disconnect(m_c);
     }
 
@@ -22,9 +24,32 @@ class connection {
       return m_c;
     }
 
+    xcb_screen_t * const default_screen_of_display(void)
+    {
+      return m_default_screen_of_display;
+    }
+
+    xcb_window_t root(void)
+    {
+      return m_default_screen_of_display->root;
+    }
+
   private:
+    xcb_screen_t * screen_of_display(int screen)
+    {
+      xcb_screen_iterator_t iter;
+
+      iter = xcb_setup_roots_iterator(xcb_get_setup(m_c));
+      for (; iter.rem; --screen, xcb_screen_next (&iter))
+        if (screen == 0)
+          return iter.data;
+
+      return NULL;
+    }
+
     xcb_connection_t * m_c = NULL;
     int m_default_screen_number = 0;
+    xcb_screen_t * m_default_screen_of_display = NULL;
 };
 
 };
