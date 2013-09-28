013fa82a8a669183bb1c327b639a247e59911590
Jochen Keil <jochen.keil@gmail.com>
2013-09-28 13:08:08 +0200

    Make inheritance public

--- requests.hpp
+++ requests.hpp
@@ -8,9 +8,9 @@ namespace x {
 namespace request {
 
 class query_tree
-  : generic::request<xcb_query_tree_cookie_t,
-                     xcb_query_tree_reply_t,
-                     &xcb_query_tree_reply>
+  : public generic::request<xcb_query_tree_cookie_t,
+                            xcb_query_tree_reply_t,
+                            &xcb_query_tree_reply>
 {
   public:
     query_tree(xcb_connection_t * c, xcb_window_t window)
