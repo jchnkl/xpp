9470d50286146703a6c58b6e5ad81f8a2c7ac75b
Jochen Keil <jochen.keil@gmail.com>
2013-12-24 10:28:04 +0100

    Utility macros for semi-automagical request generation

--- src/requests.hpp
+++ src/requests.hpp
@@ -1,7 +1,30 @@
 #ifndef X_REQUESTS_HPP
 #define X_REQUESTS_HPP
 
+#include "macros.hpp"
 #include "request.hpp"
+
+#define REQUEST_PROT(NAMESPACE, NAME, ...)                                    \
+namespace NAMESPACE {                                                         \
+class NAME : public generic::request<xcb_ ## NAME ## _cookie_t,               \
+                            xcb_ ## NAME ## _reply_t,                         \
+                            &xcb_ ## NAME ## _reply>                          \
+{                                                                             \
+  public:                                                                     \
+    NAME(xcb_connection_t * c, MACRO_DISPATCHER(TYPE_ARG_CC, __VA_ARGS__));   \
+};                                                                            \
+};
+
+#define REQUEST_BODY(NAMESPACE, NAME, ...)                                    \
+NAMESPACE::NAME::NAME(xcb_connection_t * c,                                   \
+                      MACRO_DISPATCHER(TYPE_ARG_CC, __VA_ARGS__))             \
+  : request(c, &xcb_ ## NAME,
+
+#define SIMPLE_REQUEST(NAMESPACE, NAME, ...)                                  \
+  REQUEST_PROT(NAMESPACE, NAME, __VA_ARGS__)                                  \
+  REQUEST_BODY(NAMESPACE, NAME, __VA_ARGS__)                                  \
+  MACRO_DISPATCHER(ARGN_PASTER, __VA_ARGS__))                                 \
+{}
 
 namespace xpp {
 
