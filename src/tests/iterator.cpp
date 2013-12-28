4a1a66273208df0813f4c1360ed40df62e76bed6
Jochen Keil <jochen.keil@gmail.com>
2013-12-28 22:13:09 +0100

    Trying out iterator code

--- src/tests/iterator.cpp
+++ src/tests/iterator.cpp
@@ -2,31 +2,132 @@
 
 #include "../connection.hpp"
 
-using namespace xpp;
+class test {
+  public:
+
+    template<typename Data>
+    class iterator {
+      public:
+        iterator(Data * const data, std::size_t index)
+          : m_data(data), m_index(index)
+        {}
+
+        bool operator==(const iterator & other)
+        {
+          return m_index == other.m_index;
+        }
+
+        bool operator!=(const iterator & other)
+        {
+          return ! (*this == other);
+        }
+
+        const Data & operator*(void)
+        {
+          return m_data[m_index];
+        }
+
+        // prefix
+        iterator & operator++(void)
+        {
+          ++m_index;
+          return *this;
+        }
+
+        // postfix
+        iterator operator++(int)
+        {
+          auto copy = *this;
+          ++(*this);
+          return copy;
+        }
+
+        // prefix
+        iterator & operator--(void)
+        {
+          --m_index;
+          return *this;
+        }
+
+        // postfix
+        iterator operator--(int)
+        {
+          auto copy = *this;
+          --(*this);
+          return copy;
+        }
+
+      private:
+        Data * const m_data;
+        std::size_t m_index = 0;
+    };
+
+
+    template<typename T>
+    iterator<T> begin(void)
+    {
+      throw "This must not happen!";
+    }
+
+    template<typename T>
+    iterator<T> end(void)
+    {
+      throw "This must not happen!";
+    }
+
+  // private:
+    std::vector<int> m_ints;
+    std::vector<double> m_doubles;
+
+};
+
+template<>
+test::iterator<int> test::begin<int>(void)
+{
+  return iterator<int>(m_ints.data(), 0);
+}
+
+template<>
+test::iterator<double> test::begin<double>(void)
+{
+  return iterator<double>(m_doubles.data(), 0);
+}
+
+template<>
+test::iterator<int> test::end<int>(void)
+{
+  return iterator<int>(m_ints.data(), m_ints.size());
+}
+
+template<>
+test::iterator<double> test::end<double>(void)
+{
+  return iterator<double>(m_doubles.data(), m_ints.size());
+}
 
 int main(int argc, char ** argv)
 {
-  connection c("");
+  xpp::connection c("");
 
-  auto reply = c.query_tree(c.root());
+  auto tree = c.query_tree(c.root());
 
-  std::cerr << "#windows (children_len): " << reply->children_len << std::endl;
-  std::cerr << "#windows (length):       " << reply->length << std::endl;
+  std::cerr << "#windows (children_len): " << tree->children_len << std::endl;
+  std::cerr << "#windows (length):       " << tree->length << std::endl;
 
   std::cerr << std::hex;
-  for (auto & window : reply) {
+  for (auto & window : tree.children()) {
     std::cerr << "0x" << window << "; ";
   }
   std::cerr << std::dec << std::endl;;
 
   std::cerr << std::hex;
-  for (auto it = reply.begin(); it != reply.end(); ++it) {
+  for (auto it = tree.children().begin(); it != tree.children().end(); ++it) {
     std::cerr << "0x" << *it << "; ";
   }
   std::cerr << std::dec << std::endl;;
 
   std::cerr << std::hex;
-  auto it = reply.begin();
+  auto it = tree.children().begin();
   std::cerr << "it  : " << *it     << std::endl;
   std::cerr << "++it: " << *(++it) << std::endl;
   std::cerr << "it  : " << *it     << std::endl;
