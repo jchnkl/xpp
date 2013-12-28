0a5f4e66702ebaa859df4413dd9c6147b0e850b9
Jochen Keil <jochen.keil@gmail.com>
2013-12-28 22:02:57 +0100

    Remove iterator macro

--- src/request_macros.hpp
+++ src/request_macros.hpp
@@ -23,68 +23,6 @@ NAMESPACE::NAME::NAME(xcb_connection_t * c                                    \
   MACRO_DISPATCHER(ARGN_PASTER, __VA_ARGS__))                                 \
 {}
 
-#define ITERATOR(NAME, MEMBER)                                                \
-template<typename Member>                                                     \
-class iterator {                                                              \
-public:                                                                       \
-    iterator(xcb_ ## NAME ## _reply_t * const reply, bool begin)              \
-      : m_reply(reply)                                                        \
-    {                                                                         \
-      m_elements =                                                            \
-        static_cast<Member *>(xcb_ ## NAME ## _ ## MEMBER (m_reply));         \
-      if (! begin) m_position = m_reply-> MEMBER ## _len;                     \
-    }                                                                         \
-                                                                              \
-    bool operator==(const iterator & other)                                   \
-    {                                                                         \
-      return m_position == other.m_position;                                  \
-    }                                                                         \
-                                                                              \
-    bool operator!=(const iterator & other)                                   \
-    {                                                                         \
-      return ! (*this == other);                                              \
-    }                                                                         \
-                                                                              \
-    const Member & operator*(void)                                            \
-    {                                                                         \
-      return m_elements[m_position];                                          \
-    }                                                                         \
-                                                                              \
-    /* prefix */                                                              \
-    iterator & operator++(void)                                               \
-    {                                                                         \
-      ++m_position;                                                           \
-      return *this;                                                           \
-    }                                                                         \
-                                                                              \
-    /* postfix */                                                             \
-    iterator operator++(int)                                                  \
-    {                                                                         \
-      auto copy = *this;                                                      \
-      ++(*this);                                                              \
-      return copy;                                                            \
-    }                                                                         \
-                                                                              \
-    /* prefix */                                                              \
-    iterator & operator--(void)                                               \
-    {                                                                         \
-      --m_position;                                                           \
-      return *this;                                                           \
-    }                                                                         \
-                                                                              \
-    /* postfix */                                                             \
-    iterator operator--(int)                                                  \
-    {                                                                         \
-      auto copy = *this;                                                      \
-      --(*this);                                                              \
-      return copy;                                                            \
-    }                                                                         \
-                                                                              \
-  private:                                                                    \
-    std::size_t m_position = 0;                                               \
-    Member * m_elements = NULL;                                               \
-    xcb_ ## NAME ## _reply_t * m_reply;                                       \
-};
 
 #define ITERATOR_REQUEST_PROTO_COMMON(NAME, MEMBER, TYPE, ...)\
 class NAME : public generic::request<xcb_ ## NAME ## _cookie_t,               \
