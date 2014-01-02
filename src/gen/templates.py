97637f139a981126e3ce73c0f9281a624f1ad52b
Jochen Keil <jochen.keil@gmail.com>
2014-01-02 21:28:15 +0100

    CppRequest, ParameterList & Parameter classes

--- src/gen/templates.py
+++ src/gen/templates.py
@@ -155,3 +155,260 @@ xpp::generic::string<
                (this->get());
     }\
 """ % (string, member, string)
+
+
+
+########## REQUESTS  ##########
+
+class CppRequest(object):
+    def __init__(self, name, is_void, c_namespace=""):
+        self.name = name
+        self.is_void = is_void
+        self.c_namespace = c_namespace
+        self.accessors = []
+        self.parameter_list = ParameterList()
+
+    def make_proto(self):
+        return "class " + self.name + ";"
+        # self.parameter_list.make_wrapped()
+        # return self.void_request() if self.is_void else self.reply_request()
+
+    def make_class(self):
+        self.parameter_list.make_wrapped()
+        return self.void_request() if self.is_void else self.reply_request()
+
+    def add(self, param):
+        self.parameter_list.parameter.append(param)
+
+    def comma(self):
+        return self.parameter_list.comma()
+
+    def c_name(self):
+        ns = "" if self.c_namespace == "" else self.c_namespace + "_"
+        return "xcb_" + ns + self.name
+
+    def calls(self):
+        return self.parameter_list.calls()
+
+    def protos(self):
+        return self.parameter_list.protos()
+
+    def template(self):
+        return "    template<typename " \
+                + ", typename ".join(self.parameter_list.templates) \
+                + ">\n" \
+                if len(self.parameter_list.templates) > 0 \
+                else ""
+
+
+    def wrapped_calls(self):
+        return self.parameter_list.wrapped_calls()
+
+    def wrapped_protos(self):
+        return self.parameter_list.wrapped_protos()
+
+    def make_accessors(self):
+        return "\n".join(map(lambda a: "\n%s\n" % a, self.accessors))
+
+
+########## VOID REQUEST  ##########
+
+    def void_request(self):
+        def methods(protos, calls, template=""):
+            ctor = \
+"""\
+    %s(xcb_connection_t * c%s)
+    {
+      %s(c%s);
+    }
+""" % (self.name, self.comma() + protos, \
+       self.c_name(), self.comma() + calls)
+
+            operator = \
+"""\
+    void
+    operator()(xcb_connection_t * c%s)
+    {
+      %s(c%s);
+    }
+""" % (self.comma() + protos, \
+       self.c_name(), self.comma() + calls)
+
+            return template + ctor + "\n" + template + operator
+
+
+        head = \
+"""\
+class %s {
+  public:
+    %s(void) {}
+
+""" % (self.name, self.name)
+
+        tail = \
+"""\
+}; // class %s
+""" % (self.name)
+
+
+        wrapped = "\n" \
+                + methods(self.wrapped_protos(), self.wrapped_calls(), self.template()) \
+                if self.parameter_list.want_wrap \
+                else ""
+
+        return head \
+             + methods(self.protos(), self.calls()) \
+             + wrapped \
+             + self.make_accessors() \
+             + tail
+
+########## VOID REQUEST  ##########
+
+
+
+########## REPLY_REQUEST ##########
+
+    def reply_request(self):
+        def methods(protos, calls, template=""):
+            return template + \
+"""\
+    %s(xcb_connection_t * c%s)
+      : request(c, &%s%s)
+    {}
+""" % (self.name, \
+       self.comma() + protos, \
+       self.c_name(), self.comma() + calls)
+
+        head = \
+"""\
+class %s
+  : public generic::request<%s_cookie_t,
+                            %s_reply_t,
+                            &%s_reply>
+{
+  public:
+""" % (self.name, \
+       self.c_name(), \
+       self.c_name(), \
+       self.c_name())
+
+        tail = \
+"""\
+}; // class %s\
+""" % (self.name)
+
+
+        wrapped = "\n" \
+                + methods(self.wrapped_protos(), self.wrapped_calls(), self.template()) \
+                if self.parameter_list.want_wrap \
+                else ""
+
+        return head \
+             + methods(self.protos(), self.calls()) \
+             + wrapped \
+             + self.make_accessors() \
+             + tail
+
+########## REPLY_REQUEST ##########
+
+########## REQUESTS  ##########
+
+
+
+########## PARAMETER ##########
+
+class ParameterList(object):
+    def __init__(self):
+        self.want_wrap = False
+        self.parameter = []
+        self.wrap_calls = []
+        self.wrap_protos = []
+        self.templates = []
+
+    def comma(self):
+        return "" if len(self.parameter) == 0 else ", "
+
+    def calls(self, params=None):
+        params = self.parameter if params == None else params
+        calls = map(lambda p: p.call(), params)
+        return "" if len(calls) == 0 else ", ".join(calls)
+
+    def protos(self, params=None):
+        params = self.parameter if params == None else params
+        ps = sorted(params, cmp=lambda p1, p2: cmp(p1.default, p2.default))
+        protos = map(lambda p: p.proto(), ps)
+        return "" if len(protos) == 0 else ", ".join(protos)
+
+    def make_wrapped(self):
+        self.wrap_calls = []
+        self.wrap_protos = []
+
+        for index, param in enumerate(self.parameter):
+            prev = index - 1
+
+            if (param.is_const and param.is_pointer
+                    and prev >= 0
+                    and self.parameter[prev].name == param.name + "_len"):
+
+                self.want_wrap = True
+                self.wrap_calls.pop(prev)
+                self.wrap_protos.pop(prev)
+
+                prev_type = self.parameter[prev].type
+                if param.type == 'char':
+                    self.wrap_calls.append(Parameter(
+                        name="static_cast<" + prev_type + ">(" + param.name + '.length())'))
+                    self.wrap_calls.append(
+                            Parameter(name=param.name + '.c_str()'))
+                    self.wrap_protos.append(
+                            Parameter(type='const std::string &', name=param.name))
+
+                else:
+                    param_type = param.type
+                    if param_type == "void":
+                        param_type = "Type_" + str(index)
+                        self.templates.append(param_type)
+
+                    prev_type = self.parameter[prev].type
+                    self.wrap_calls.append(Parameter(
+                        name="static_cast<" + prev_type + ">(" + param.name + '.size())'))
+                    self.wrap_calls.append(
+                            Parameter(name=param.name + '.data()'))
+                    self.wrap_protos.append(
+                            Parameter(type='const std::vector<' + param_type + '> &',
+                                      name=param.name))
+
+            else:
+                self.wrap_calls.append(param)
+                self.wrap_protos.append(param)
+
+    def wrapped_calls(self):
+        return self.calls(self.wrap_calls)
+
+    def wrapped_protos(self):
+        return self.protos(self.wrap_protos)
+
+
+
+_default_parameter_values =\
+    { "xcb_timestamp_t" : "XCB_TIME_CURRENT_TIME" }
+
+class Parameter(object):
+    def __init__(self, type="", name="", is_const=False, is_pointer=False):
+        self.type = type
+        self.name = name
+        self.is_const = is_const
+        self.is_pointer = is_pointer
+        self.default = _default_parameter_values.get(self.type)
+
+    def call(self):
+        return self.name
+
+    def proto(self):
+        type = ("const " if self.is_const else "") \
+             + self.type \
+             + (" *" if self.is_pointer else "")
+        return type + " " + self.name \
+                + ("" if self.default == None else " = " + self.default)
+
+########## PARAMETER ##########
