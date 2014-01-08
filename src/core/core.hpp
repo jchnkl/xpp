892aec8385310e6ad880450451d03b363572ff9e
Jochen Keil <jochen.keil@gmail.com>
2014-01-08 12:38:36 +0100

    Make *all* the methods const

--- src/core/core.hpp
+++ src/core/core.hpp
@@ -43,64 +43,66 @@ class core {
       return m_c.get();
     }
 
-    int flush(void)
+    int flush(void) const
     {
       return xcb_flush(m_c.get());
     }
 
-    uint32_t get_maximum_request_length(void)
+    uint32_t get_maximum_request_length(void) const
     {
       return xcb_get_maximum_request_length(m_c.get());
     }
 
-    void prefetch_maximum_request_length(void)
+    void prefetch_maximum_request_length(void) const
     {
       xcb_prefetch_maximum_request_length(m_c.get());
     }
 
-    xcb_generic_event_t * wait_for_event(void)
+    xcb_generic_event_t * wait_for_event(void) const
     {
       return xcb_wait_for_event(m_c.get());
     }
 
-    xcb_generic_event_t * poll_for_event(void)
+    xcb_generic_event_t * poll_for_event(void) const
     {
       return xcb_poll_for_event(m_c.get());
     }
 
-    xcb_generic_event_t * poll_for_queued_event(void)
+    xcb_generic_event_t * poll_for_queued_event(void) const
     {
       return xcb_poll_for_queued_event(m_c.get());
     }
 
-    xcb_generic_event_t * poll_for_special_event(xcb_special_event_t * se)
+    xcb_generic_event_t *
+      poll_for_special_event(xcb_special_event_t * se) const
     {
       return xcb_poll_for_special_event(m_c.get(), se);
     }
 
-    xcb_generic_event_t * wait_for_special_event(xcb_special_event_t * se)
+    xcb_generic_event_t *
+      wait_for_special_event(xcb_special_event_t * se) const
     {
       return xcb_wait_for_special_event(m_c.get(), se);
     }
 
     xcb_special_event_t * register_for_special_xge(xcb_extension_t * ext,
                                                    uint32_t eid,
-                                                   uint32_t * stamp)
+                                                   uint32_t * stamp) const
     {
       return xcb_register_for_special_xge(m_c.get(), ext, eid, stamp);
     }
 
-    void unregister_for_special_event(xcb_special_event_t * se)
+    void unregister_for_special_event(xcb_special_event_t * se) const
     {
       xcb_unregister_for_special_event(m_c.get(), se);
     }
 
-    xcb_generic_error_t * request_check(xcb_void_cookie_t cookie)
+    xcb_generic_error_t * request_check(xcb_void_cookie_t cookie) const
     {
       return xcb_request_check(m_c.get(), cookie);
     }
 
-    void discard_reply(unsigned int sequence)
+    void discard_reply(unsigned int sequence) const
     {
       xcb_discard_reply(m_c.get(), sequence);
     }
@@ -108,39 +110,39 @@ class core {
     // The result must not be freed.
     // This storage is managed by the cache itself.
     const xcb_query_extension_reply_t * const
-      get_extension_data(xcb_extension_t * ext)
+      get_extension_data(xcb_extension_t * ext) const
     {
       return xcb_get_extension_data(m_c.get(), ext);
     }
 
-    void prefetch_extension_data(xcb_extension_t * ext)
+    void prefetch_extension_data(xcb_extension_t * ext) const
     {
       xcb_prefetch_extension_data(m_c.get(), ext);
     }
 
-    const xcb_setup_t * const get_setup(void)
+    const xcb_setup_t * const get_setup(void) const
     {
       return xcb_get_setup(m_c.get());
     }
 
-    int get_file_descriptor(void)
+    int get_file_descriptor(void) const
     {
       return xcb_get_file_descriptor(m_c.get());
     }
 
-    int connection_has_error(void)
+    int connection_has_error(void) const
     {
       return xcb_connection_has_error(m_c.get());
     }
 
-    void disconnect(void)
+    void disconnect(void) const
     {
       xcb_disconnect(m_c.get());
     }
 
     // hostname, display, screen
     std::tuple<std::string, int, int>
-    parse_display(const std::string & name)
+      parse_display(const std::string & name) const
     {
       int screen = 0;
       int display = 0;
@@ -155,7 +157,7 @@ class core {
       return std::make_tuple(hostname, display, screen);
     }
 
-    uint32_t generate_id(void)
+    uint32_t generate_id(void) const
     {
       return xcb_generate_id(m_c.get());
     }
