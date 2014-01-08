82c1e13021ccc48e435f90dda485513d1ede2ad4
Jochen Keil <jochen.keil@gmail.com>
2014-01-08 12:40:45 +0100

    Base class for loading extensions

--- src/core/core.hpp
+++ src/core/core.hpp
@@ -182,6 +182,42 @@ class connection
     {}
 }; // class connection
 
+template<xcb_extension_t * ID>
+class extension {
+  public:
+    extension(const core & core)
+      : m_core(core)
+    {
+      m_core.prefetch_extension_data(ID);
+    }
+
+    void get(void)
+    {
+      m_extension = m_core.get_extension_data(ID);
+    }
+
+    uint8_t major_opcode(void)
+    {
+      return m_extension->major_opcode;
+    }
+
+    uint8_t first_event(void)
+    {
+      return m_extension->first_event;
+    }
+
+    uint8_t first_error(void)
+    {
+      return m_extension->first_error;
+    }
+
+  private:
+    const core & m_core;
+    // The result must not be freed.
+    // This storage is managed by the cache itself.
+    const xcb_query_extension_reply_t * m_extension;
+}; // class extension
+
 }; // namespace xpp
 
 #endif // XPP_CORE_HPP
