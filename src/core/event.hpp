b15d5ef82ea3e03e52eefe6e824bb38c0aa3adee
Jochen Keil <jochen.keil@gmail.com>
2014-01-24 12:59:46 +0100

    Generic event interface / template

---
+++ src/core/event.hpp
@@ -0,0 +1,77 @@
+#ifndef XPP_EVENT_HPP
+#define XPP_EVENT_HPP
+
+#include <memory> // shared_ptr
+#include <xcb/xcb.h> // xcb_generic_event_t
+
+namespace xpp {
+
+/*
+namespace xcb {
+
+class handler {
+  public:
+    template<typename Event> void handle(const Event &);
+}; // class handler
+
+class dispatcher {
+  public:
+    template<typename Handler>
+    static
+    bool
+    dispatch_with(const handler &, xcb_generic_event_t * event);
+}; // class dispatcher
+
+}; // namespace xcb
+*/
+
+namespace event {
+
+template<int OpCode, typename Event>
+class generic {
+  public:
+    generic(Event * event)
+      : m_event(event)
+    {}
+
+    generic(xcb_generic_event_t * event)
+      // : m_event(static_cast<Event *>(event))
+      : m_event((Event *)(event))
+    {}
+
+    virtual
+    ~generic(void) {}
+
+    virtual
+    operator Event &(void) const
+    {
+      return *m_event;
+    }
+
+    virtual
+    const Event &
+    operator*(void) const
+    {
+      return *m_event;
+    }
+
+    virtual
+    Event * const
+    operator->(void) const
+    {
+      return m_event.get();
+    }
+
+    static const int opcode = OpCode;
+
+  protected:
+    std::shared_ptr<Event> m_event;
+}; // class generic
+
+}; // namespace event
+
+}; // namespace xpp
+
+// }; }; // xpp::event
+
+#endif // XPP_EVENT_HPP
