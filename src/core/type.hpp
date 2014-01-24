62d51f2579d11b90662ca017b7a55f5480926fcf
Jochen Keil <jochen.keil@gmail.com>
2014-01-24 21:59:19 +0100

    A generic type wrapper

---
+++ src/core/type.hpp
@@ -0,0 +1,22 @@
+#ifndef XPP_XCB_TYPE_HPP
+#define XPP_XCB_TYPE_HPP
+
+// #include <xcb/xcb.h>
+#include <cstddef> // size_t
+
+namespace xpp {
+
+namespace xcb {
+
+template<typename Type>
+class type {
+  public:
+    virtual ~type(void) {}
+    virtual operator Type(void) const = 0;
+}; // class type
+
+}; // namespace xcb
+
+}; // namespace xpp
+
+#endif // XPP_XCB_TYPE_HPP
