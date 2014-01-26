87e10b1fba06c7b928819548bf2d51a7b8d4a680
Jochen Keil <jochen.keil@gmail.com>
2014-01-26 23:22:39 +0100

    Add all xidtypes to _resource_classes
    grep -E 'xidtype|xidunion' *.xml

--- src/gen/templates.py
+++ src/gen/templates.py
@@ -56,23 +56,85 @@ import sys # sys.stderr.write
 import copy # deepcopy
 
 _resource_classes = \
-    { "DRAWABLE"
-    , "WINDOW"
+    {
+    ### XPROTO ###
+      "WINDOW"
     , "PIXMAP"
-    , "ATOM"
     , "CURSOR"
     , "FONT"
     , "GCONTEXT"
+    , "COLORMAP"
+    , "ATOM"
+    , "DRAWABLE"
     , "FONTABLE"
-### XPROTO ###
+    ### XPROTO ###
 
-### RANDR ###
+    ### DAMAGE ###
+    , "DAMAGE"
+    ### DAMAGE ###
+
+    ### GLX, RECORD, XVMC ###
+    , "CONTEXT"
+    ### GLX, RECORD, XVMC ###
+
+    ### GLX ###
+    # , "PIXMAP" # already in XPROTO
+    # , "CONTEXT"
+    , "PBUFFER"
+    # , "WINDOW" # already in XPROTO
+    , "FBCONFIG"
+    ### GLX ###
+
+    ### PRESENT ###
+    , "EVENT"
+    ### PRESENT ###
+
+    ### RANDR ###
     , "MODE"
     , "CRTC"
     , "OUTPUT"
     , "PROVIDER"
-### RANDR ###
+    ### RANDR ###
 
+    ### RECORD ###
+    # , "CONTEXT"
+    ### RECORD ###
+
+    ### RENDER ###
+    , "GLYPHSET"
+    , "PICTURE"
+    , "PICTFORMAT"
+    ### RENDER ###
+
+    ### SHM ###
+    , "SEG"
+    ### SHM ###
+
+    ### SYNC ###
+    , "ALARM"
+    , "COUNTER"
+    , "FENCE"
+    ### SYNC ###
+
+    ### XFIXES ###
+    , "REGION"
+    , "BARRIER"
+    ### XFIXES ###
+
+    ### XPRINT ###
+    , "PCONTEXT"
+    ### XPRINT ###
+
+    ### XVMC ###
+    # , "CONTEXT"
+    , "SURFACE"
+    , "SUBPICTURE"
+    ### XVMC ###
+
+    ### XV ###
+    , "PORT"
+    , "ENCODING"
+    ### XV ###
     }
 
 def get_namespace(namespace):
