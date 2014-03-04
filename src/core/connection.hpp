e4edf1617e5d1b8de83dc41767954ed8df79bad0
Jochen Keil <jochen.keil@gmail.com>
2014-03-04 11:10:36 +0100

    Cleanup

--- src/core/connection.hpp
+++ src/core/connection.hpp
@@ -2,16 +2,10 @@
 #define XPP_CONNECTION_HPP
 
 #include "core.hpp"
-// #include "type.hpp"
-#include "window.hpp"
-#include "extension.hpp"
-// #include "core/connection.hpp"
 #include "../factory.hpp"
 #include "generic/connection.hpp"
 
-// #include "../gen/xproto-stub.hpp"
 #include "../gen/xproto.hpp"
-// #include "../gen/protos.hpp"
 
 namespace xpp {
 
@@ -27,21 +21,8 @@ class connection
   protected:
     typedef connection<Extensions ...> self;
 
-    // virtual const self & get(void) const
-    // {
-    //   // return const_cast<self &>(*this);
-    //   return *this;
-    // }
-
     virtual self & get(void)
     {
-      // return const_cast<self &>(*this);
-      return *this;
-    }
-
-    virtual const self & get(void) const
-    {
-      // return const_cast<self &>(*this);
       return *this;
     }
 
@@ -50,50 +31,21 @@ class connection
     explicit
     connection(Parameters && ... parameters)
       : xpp::core::core(std::forward<Parameters>(parameters) ...)
-      // , m_root_window(static_cast<const core &>(*this))
-      // , m_root_window(*this)
     {
       prefetch_data<Extensions ...>();
       get_data<Extensions ...>();
       m_root_window = screen_of_display(default_screen())->root;
     }
 
-    // virtual
-    // ~connection(void)
-    // {}
-
-    // virtual
-    // operator core &(void)
-    // {
-    //   return static_cast<core &>(*this);
-    // }
-
-    // virtual
-    // operator const core &(void) const
-    // {
-    //   return static_cast<const core &>(*this);
-    // }
+    virtual
+    ~connection(void)
+    {}
 
     virtual
     operator xcb_connection_t * const(void) const
     {
       return *(static_cast<const core &>(*this));
     }
-
-    template<typename Extension>
-    operator const connection<Extension> &(void) const
-    {
-      return reinterpret_cast<const connection<Extension> &>(*this);
-      // return static_cast<connection<> &>(*this);
-      // return *this;
-    }
-
-    // operator const connection<> &(void) const
-    // {
-    //   return reinterpret_cast<const connection<> &>(*this);
-    //   // return static_cast<connection<> &>(*this);
-    //   // return *this;
-    // }
 
     // TODO
     // virtual operator Display * const(void) const
@@ -109,7 +61,6 @@ class connection
     }
 
   private:
-    // xpp::window<self &> m_root_window;
     xcb_window_t m_root_window;
 
     template<typename Extension, typename Next, typename ... Rest>
@@ -148,18 +99,9 @@ template<>
 template<typename ... Parameters>
 connection<>::connection(Parameters && ... parameters)
   : xpp::core::core(std::forward<Parameters>(parameters) ...)
-  // , m_root_window(static_cast<const core &>(*this))
-  // , m_root_window(*this)
 {
   m_root_window = screen_of_display(static_cast<core &>(*this).default_screen())->root;
 }
-
-// template<typename ... Extensions>
-// template<>
-// connection<Extensions ...>::operator const connection<> &(void) const
-// {
-//   return reinterpret_cast<const connection<> &>(*this);
-// }
 
 }; // namespace xpp
 
