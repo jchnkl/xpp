1f8ccdde41e76c2f0e8b0e08629e75920cfeb6d6
Jochen Keil <jochen.keil@gmail.com>
2014-03-04 22:58:55 +0100

    Remove more dead code

--- src/gen/cppcookie.py
+++ src/gen/cppcookie.py
@@ -68,22 +68,18 @@ def _void_cookie_function(name, c_name, template, return_value, protos, calls, i
     if len(template) == 0: template = "template<typename Connection>\n"
     return _templates['void_cookie_function'] % \
             ( template
-            # , return_value
             , name
             , protos
             , initializer
             , c_name
             , calls
-            # checked
             , template
-            # , return_value
             , name
             , protos
             , initializer
             , c_name
             , calls
             )
-
 
 _templates['cookie_static_getter'] = \
 '''\
@@ -126,13 +122,6 @@ class CppCookie(object):
     def protos(self, sort, defaults):
         return self.parameter_list.protos(sort, defaults)
 
-    def template(self, indent="    ", tail="\n"):
-        return indent + "template<typename " \
-                + ", typename ".join(self.parameter_list.templates) \
-                + ">" + tail \
-                if len(self.parameter_list.templates) > 0 \
-                else ""
-
     def iterator_template(self, indent="    ", tail="\n"):
         prefix = "template<typename " + ("Connection, typename " if self.is_void else "")
         return indent + prefix \
@@ -141,7 +130,6 @@ class CppCookie(object):
                 + ">" + tail \
                 if len(self.parameter_list.iterator_templates) > 0 \
                 else ""
-
 
     def iterator_calls(self, sort):
         return self.parameter_list.iterator_calls(sort)
@@ -249,46 +237,5 @@ class CppCookie(object):
         result = ""
         if len(default_args + wrapped) > 0:
             result += "\n" + default + default_args + wrapped
-
-        return result
-
-    def make(self):
-
-        default = self.methods(self.protos(False, False), self.calls(False))
-
-        if self.parameter_list.has_defaults:
-            default = self.methods(self.protos(True, True), self.calls(False))
-
-        wrapped = ""
-        if self.parameter_list.want_wrap:
-            wrapped = "\n" + \
-                self.methods(self.iterator_protos(True, True),
-                        self.iterator_calls(False), self.iterator_template(),
-                        self.iterator_initializers())
-
-        default_args = ""
-        if self.parameter_list.is_reordered():
-            default_args = "\n" + \
-                self.methods(self.protos(True, True), self.calls(False))
-
-
-        result = ""
-        if len(default_args + wrapped) > 0:
-            result += "namespace generic {\n"
-            result += "namespace cookie {\n"
-
-
-            if self.is_void:
-                result += _void_cookie_class_derived(self.request_name, default + default_args + wrapped)
-            else:
-                result += _reply_cookie_class_derived(self.request_name, default + default_args + wrapped)
-
-            result += "}; // cookie\n"
-            result += "}; // generic\n"
-
-            result += _cookie_checked_unchecked_derived(self.request_name, self.is_void)
-
-        else:
-            result += _cookie_checked_unchecked(self.request_name, self.is_void)
 
         return result
