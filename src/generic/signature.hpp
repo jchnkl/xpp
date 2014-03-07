a9fb26f4711086dc77396b222415a40fca885b75
Jochen Keil <jochen.keil@gmail.com>
2014-03-07 11:44:46 +0100

    Move macro to top of file

--- src/generic/signature.hpp
+++ src/generic/signature.hpp
@@ -1,13 +1,13 @@
 #ifndef XPP_GENERIC_SIGNATURE_HPP
 #define XPP_GENERIC_SIGNATURE_HPP
 
+#define SIGNATURE(NAME) \
+  xpp::generic::signature<decltype(NAME), NAME>
+
 namespace xpp { namespace generic {
 
 template<typename Signature, Signature & S>
 class signature;
-
-#define SIGNATURE(NAME) \
-  xpp::generic::signature<decltype(NAME), NAME>
 
 }; };
 
