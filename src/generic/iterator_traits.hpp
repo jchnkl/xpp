a619d7ef2af5a60d1c36472761d19fa886286604
Jochen Keil <jochen.keil@gmail.com>
2014-03-11 21:26:47 +0100

    Remove old iterable interface

--- src/generic/iterator_traits.hpp
+++ src/generic/iterator_traits.hpp
@@ -1,8 +1,6 @@
 #ifndef XPP_GENERIC_ITERATOR_TRAITS_HPP
 #define XPP_GENERIC_ITERATOR_TRAITS_HPP
 
-#include <cstdlib> // size_t
-
 namespace xpp {
 
 namespace generic {
@@ -20,23 +18,6 @@ struct conversion_type
 };
 
 }; // namespace generic
-
-// interface for object types to be used with an itertor
-
-template<typename Type>
-class iterable {
-  public:
-    virtual ~iterable(void) {}
-    virtual void operator=(Type) = 0;
-}; // class iterable
-
-template<>
-class iterable<void> {
-  public:
-    virtual ~iterable(void) {}
-    static std::size_t size_of(void);
-    virtual void operator=(void * const) = 0;
-};
 
 };
 
