99bcfbe571d60c9ec7844e784b07c90b95efcf1c
Jochen Keil <jochen.keil@gmail.com>
2014-03-12 12:49:40 +0100

    Add all generic files

--- src/generic.hpp
+++ src/generic.hpp
@@ -1,13 +1,16 @@
 #ifndef XPP_GENERIC_HPP
 #define XPP_GENERIC_HPP
 
-#include "generic/event.hpp"
+#include "generic/type.hpp"
 #include "generic/error.hpp"
+#include "generic/event.hpp"
 #include "generic/factory.hpp"
 #include "generic/request.hpp"
 #include "generic/extension.hpp"
+#include "generic/signature.hpp"
 #include "generic/connection.hpp"
 #include "generic/reply_iterator.hpp"
+#include "generic/iterator_traits.hpp"
 #include "generic/input_iterator_adapter.hpp"
 
 #endif // XPP_GENERIC_HPP
