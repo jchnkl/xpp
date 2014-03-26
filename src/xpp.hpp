fad9899fe24c01f0e4ee133049676c3e05e388fb
Jochen Keil <jochen.keil@gmail.com>
2014-03-26 12:20:31 +0100

    Add wrapper classes for basic resource types

--- src/xpp.hpp
+++ src/xpp.hpp
@@ -2,8 +2,18 @@
 #define XPP_HPP
 
 #include "generic.hpp"
-#include "event.hpp"
+
+#include "atom.hpp"
+#include "colormap.hpp"
+#include "cursor.hpp"
+#include "drawable.hpp"
+#include "font.hpp"
+#include "fontable.hpp"
+#include "gcontext.hpp"
+#include "pixmap.hpp"
 #include "window.hpp"
+
+#include "event.hpp"
 #include "connection.hpp"
 
 #endif // XPP_HPP
